2919b41c1b7949396ac9cf1f79b6ccc61da7a39b
Noooooorth <aellenf@163.com>
2019-04-30 15:24:25 +0800

    modify(87/187): modify color.sh

--- color.sh
+++ color.sh
@@ -3,11 +3,12 @@
 cd lyric
 echo Total: `ls *.md | wc -l`
 echo Done: `ls _*.md | wc -l`
-sed -i "s/前奏/<font colot=green>前奏<\/font>/g" _*.md
-sed -i "s/间奏/<font colot=green>间奏<\/font>/g" _*.md
-sed -i "s/尾奏/<font colot=green>尾奏<\/font>/g" _*.md
-sed -i "s/A番/<font colot=green>A番<\/font>/g" _*.md
-sed -i "s/B番/<font colot=green>B番<\/font>/g" _*.md
-sed -i "s/C番/<font colot=green>C番<\/font>/g" _*.md
-sed -i "s/「/<font colot=blue>「/g" _*.md
-sed -i "s/」/」<\/font>/g" _*.md
+sed -i "" "s/^前奏$/<font color=green>前奏<\/font>/g" _*.md
+sed -i "" "s/^间奏$/<font color=green>间奏<\/font>/g" _*.md
+sed -i "" "s/^尾奏$/<font color=green>尾奏<\/font>/g" _*.md
+sed -i "" "s/^副歌$/<font color=green>副歌<\/font>/g" _*.md
+sed -i "" "s/^A番$/<font color=green>A番<\/font>/g" _*.md
+sed -i "" "s/^B番$/<font color=green>B番<\/font>/g" _*.md
+sed -i "" "s/^C番$/<font color=green>C番<\/font>/g" _*.md
+sed -i "" "s/「/<font color=blue>「/g" _*.md
+sed -i "" "s/」/」<\/font>/g" _*.md
